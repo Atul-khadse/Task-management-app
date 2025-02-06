6ac2f5330ab21eb577f9baeff84c98c61e250228
Atul_khadse <khadseatul078@gmail.com>
2025-02-06 13:06:27 +0530

    auth middleware add

---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const userModel = require("../models/user.model");
+
+
+module.exports.authUser = async (req ,res , next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+    if(!token){
+        return res.status(401).json({ message: "Unauthorized"});
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await userModel.findById(decoded._id);
+        req.user = user;
+        return next();
+    } catch (error) {
+        
+    }
+
+}
